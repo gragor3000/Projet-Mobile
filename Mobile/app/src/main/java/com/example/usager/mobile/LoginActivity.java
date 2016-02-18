d13c91a44e4b44fb3a211f1ee69f5c5205e801eb
Michaël Légaré <gragor30@gmail.com>
2016-02-18 17:49:59 -0500

    ajout du hambuger dans la toolbar

--- Mobile/app/src/main/java/com/example/usager/mobile/LoginActivity.java
+++ Mobile/app/src/main/java/com/example/usager/mobile/LoginActivity.java
@@ -25,10 +25,13 @@ public class LoginActivity extends AppCompatActivity
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
+        toolbar.setTitleTextColor(getResources().getColor(R.color.textColorPrimary));
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")

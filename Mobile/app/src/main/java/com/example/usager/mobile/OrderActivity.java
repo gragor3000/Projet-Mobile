432550265846f99c3bdb8b0418e1c9d005f62220
Patriack <patricklimogesv4si@gmail.com>
2016-02-24 17:22:16 -0500

    Rajout structure de donnée contenir repas

--- Mobile/app/src/main/java/com/example/usager/mobile/OrderActivity.java
+++ Mobile/app/src/main/java/com/example/usager/mobile/OrderActivity.java
@@ -4,8 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
 
 public class OrderActivity extends AppCompatActivity {
+
+    private List<Integer> LstRepas = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +39,7 @@ public class OrderActivity extends AppCompatActivity {
 
 
     /*Permet d'ajouter un nouveau plat a la commande*/
-    private void Add_Click(View vue){
+    private void AddClick(View vue){
 
         //Recupère la référence au conteneur en mode paysage
         View Menu = (View) findViewById(R.id.CtnMenu);
@@ -44,4 +49,21 @@ public class OrderActivity extends AppCompatActivity {
            // Intent iMenu = new Intent (this, com.example.usager.mobile.) //Appeler le fragment contenant les repas lister
         }
     }
+
+
+    /*Permet d'ajouter un plat à la liste*/
+    private void AjouterRepas(int RepasID){
+
+        LstRepas.add(RepasID);
+
+        //Trouve le bouton de changement de statut
+        Button BoutonNext = (Button) findViewById(R.id.NextBtn);
+
+        //Si le boutton affiche "Bill" faire l'appel d'ajout après commande
+        if(BoutonNext.getText().toString().equalsIgnoreCase(getString(R.string.Order_End))){
+
+        }
+    }
+
+
 }

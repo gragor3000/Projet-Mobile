291c32cc54dbb1c5fbd5be6a5da395ad2ffe2a8c
Michaël Légaré <gragor30@gmail.com>
2016-02-26 09:03:04 -0500

    ajout de la classe pour stocker le username et le password

---
+++ Mobile/app/src/main/java/com/example/usager/mobile/Shared.java
@@ -0,0 +1,9 @@
+package com.example.usager.mobile;
+
+/**
+ * Created by 1253250 on 26/02/2016.
+ */
+public class Shared {
+    public static String username;
+    public static String password;
+}

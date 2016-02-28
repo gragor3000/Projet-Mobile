18b7b070ab1c64a354c8349472dd86c999fe291c
Michaël Légaré <gragor30@gmail.com>
2016-02-28 17:22:26 -0500

    ajout du bouton de paiement par paypal (fonctionnel)

--- Mobile/app/src/main/java/com/example/usager/mobile/PayBillActivity.java
+++ Mobile/app/src/main/java/com/example/usager/mobile/PayBillActivity.java
@@ -1,5 +1,8 @@
 package com.example.usager.mobile;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -7,16 +10,54 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.braintreepayments.api.BraintreeFragment;
+import com.braintreepayments.api.BraintreePaymentActivity;
+import com.braintreepayments.api.exceptions.InvalidArgumentException;
+import com.braintreepayments.api.interfaces.PaymentMethodNonceCreatedListener;
+import com.braintreepayments.api.models.PayPalConfiguration;
+import com.braintreepayments.api.models.PaymentMethodNonce;
+import com.paypal.android.sdk.payments.PayPalItem;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalPaymentDetails;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 
 public class PayBillActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String CONFIG_ENVIRONMENT = com.paypal.android.sdk.payments.PayPalConfiguration.ENVIRONMENT_NO_NETWORK;
+
+    // note that these credentials will differ between live & sandbox environments.
+    private static final String CONFIG_CLIENT_ID = "credential from developer.paypal.com";
+
+    private static final int REQUEST_CODE_PAYMENT = 1;
+    private static final int REQUEST_CODE_FUTURE_PAYMENT = 2;
+    private static final int REQUEST_CODE_PROFILE_SHARING = 3;
+
+    private static com.paypal.android.sdk.payments.PayPalConfiguration config = new com.paypal.android.sdk.payments.PayPalConfiguration()
+            .environment(CONFIG_ENVIRONMENT)
+            .clientId(CONFIG_CLIENT_ID)
+                    // The following are only used in PayPalFuturePaymentActivity.
+            .merchantName("Example Merchant")
+            .merchantPrivacyPolicyUri(Uri.parse("https://www.example.com/privacy"))
+            .merchantUserAgreementUri(Uri.parse("https://www.example.com/legal"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pay_bill);
 
+        //TOOLBAR ET NAVIGATIONDRAWER
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -31,6 +72,90 @@ public class PayBillActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         getSupportActionBar().setHomeButtonEnabled(true);
+
+        //PAYPAL
+        Intent intent = new Intent(this, PayPalService.class);
+
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config);
+
+        startService(intent);
+    }
+
+    @Override
+    public void onDestroy() {
+        stopService(new Intent(this, PayPalService.class));
+        super.onDestroy();
+    }
+
+    //paiement par paypal
+    public void onBuyPressed(View pressed) {
+
+        PayPalPayment payment1 = new PayPalPayment(new BigDecimal("6.35"), "CAD", "spaghatt",
+                PayPalPayment.PAYMENT_INTENT_SALE);
+
+
+        Intent intent = new Intent(this, PaymentActivity.class);
+
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payment1);
+
+        startActivityForResult(intent, 0);
+    }
+
+    //juste un item
+    private PayPalPayment getThingToBuy(String paymentIntent) {
+        return new PayPalPayment(new BigDecimal("1.75"), "USD", "sample item",
+                paymentIntent);
+    }
+
+    /*
+     * plusieur items affiche juste un texte mais calcul le total
+    */
+    private PayPalPayment getStuffToBuy(String paymentIntent) {
+        PayPalItem[] items =
+                {
+                        new PayPalItem("spaghat", 2, new BigDecimal("87.50"), "CAD",
+                                "sku-12345678"),
+                        new PayPalItem("paté chinois", 1, new BigDecimal("0.00"),
+                                "CAD", "sku-zero-price"),
+                        new PayPalItem("rosbeef", 6, new BigDecimal("37.99"),
+                                "CAD", "sku-33333")
+                };
+        BigDecimal subtotal = PayPalItem.getItemTotal(items);
+        BigDecimal shipping = new BigDecimal("7.21");
+        BigDecimal tax = new BigDecimal("4.67");
+
+        PayPalPaymentDetails paymentDetails = new PayPalPaymentDetails(shipping, subtotal, tax);
+        BigDecimal amount = subtotal.add(shipping).add(tax);
+
+        PayPalPayment payment = new PayPalPayment(amount, "CAD", "sample item", paymentIntent);
+        payment.items(items).paymentDetails(paymentDetails);
+        payment.custom("This is text that will be associated with the payment that the app can use.");
+
+        return payment;
+    }
+
+    //vérifie le status du paiment
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            PaymentConfirmation confirm = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+            if (confirm != null) {
+                try {
+                    Log.i("paymentExample", confirm.toJSONObject().toString(4));
+
+                } catch (JSONException e) {
+                    Log.e("paymentExample", "an extremely unlikely failure occurred: ", e);
+                }
+            }
+        }
+        else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.i("paymentExample", "The user canceled.");
+        }
+        else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID) {
+            Log.i("paymentExample", "An invalid Payment or PayPalConfiguration was submitted. Please see the docs.");
+        }
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -41,5 +166,7 @@ public class PayBillActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+
 
 }
---
+++ Mobile/app/src/main/java/com/example/usager/mobile/testActivity.java
@@ -0,0 +1,8 @@
+package com.example.usager.mobile;
+
+/**
+ * Created by Mic on 28/02/2016.
+ */
+public class testActivity {
+
+}

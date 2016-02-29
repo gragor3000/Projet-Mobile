bc9521553bee0398e61de68bfd2c528cb023468e
Patrick Limoges <patricklimogesv4si@gmail.com>
2016-02-28 20:23:26 -0500

    Avancement dans OrderActivité

--- Mobile/app/src/main/java/com/example/usager/mobile/OrderActivity.java
+++ Mobile/app/src/main/java/com/example/usager/mobile/OrderActivity.java
@@ -1,6 +1,5 @@
 package com.example.usager.mobile;
 
-import android.content.Intent;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,14 +9,18 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import java.util.List;
 
 public class OrderActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private List<Integer> LstRepas = null;
+    private List<String[][]> LstRepas = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +52,15 @@ public class OrderActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    /*Permet d'afficher la liste de plat à l'usager*/
+    private void AfficherLstRepas(){
+
+        ListView Affichage = (ListView) findViewById(R.id.CtnPanier);
+
+
+    }
+
 
     /*Permet de passez à la prochaine étape*/
     private void NextStepClick(View vue){
@@ -85,16 +97,44 @@ public class OrderActivity extends AppCompatActivity
     /*Permet d'ajouter un plat à la liste*/
     private void AjouterRepas(int RepasID){
 
-        LstRepas.add(RepasID);
+        //Si l'usager peut ajouter un plat à sa commande
+        if(PeuxAjouter(RepasID)){
 
-        //Trouve le bouton de changement de statut
-        Button BoutonNext = (Button) findViewById(R.id.NextBtn);
 
-        //Si le boutton affiche "Bill" faire l'appel d'ajout après commande
-        if(BoutonNext.getText().toString().equalsIgnoreCase(getString(R.string.Order_End))){
+            //Trouve le bouton de changement de statut
+            Button BoutonNext = (Button) findViewById(R.id.NextBtn);
 
+            //Si le boutton affiche "Bill" faire l'appel d'ajout après commande
+            if (BoutonNext.getText().toString().equalsIgnoreCase(getString(R.string.Order_End))) {
+
+            }
         }
     }
 
 
+    /*Permet d'augmenter le nombre de plat voulu*/
+    private void AjouterQuantite(int NumListe){
+
+        //LstRepas.get(NumListe)[1] += 1;
+
+    }
+
+
+    /*Permet de vérifier dans la liste de repas qu'un indice ne s'y trouve pas déjà*/
+    private boolean PeuxAjouter(int RepasID){
+
+        boolean DejaAjouter = false;
+        /*
+        //Si la liste n'est pas vide, la parcourrir pour chercher la présence du plat
+        if(LstRepas!=null){
+            for(int iList=0; iList < LstRepas.size(); iList++){
+                if(!DejaAjouter) {
+                    if(LstRepas.get(iList)[0] == RepasID){
+                        DejaAjouter = true;
+                    }
+                }
+            }
+        }               */
+        return !DejaAjouter;
+    }
 }

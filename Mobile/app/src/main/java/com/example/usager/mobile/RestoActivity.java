443ee3fca9f8c297a82b0965d6f67ed9d3664cb8
Michaël Légaré <gragor30@gmail.com>
2016-03-04 14:32:04 -0500

    finalisation de choix de resto
    marche quand même pas du au fait que l'on ne peut pas accéder a la BD

--- Mobile/app/src/main/java/com/example/usager/mobile/RestoActivity.java
+++ Mobile/app/src/main/java/com/example/usager/mobile/RestoActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,9 +15,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ExpandableListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+
+import com.braintreepayments.api.internal.HttpClient;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -27,6 +35,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+
 
 public class RestoActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -64,6 +73,27 @@ public class RestoActivity extends AppCompatActivity
         List<String> cities = new ArrayList<String>();
         List<String> restos = new ArrayList<String>();
 
+        provinces.add("province1");
+        provinces.add("province2");
+        provinces.add("province3");
+        provinces.add("province4");
+        provinces.add("province5");
+
+        cities.add("ville1");
+        cities.add("ville2");
+        cities.add("ville3");
+        cities.add("ville4");
+        cities.add("ville5");
+        cities.add("ville6");
+
+        restos.add("resto1");
+        restos.add("resto2");
+        restos.add("resto3");
+        restos.add("resto4");
+        restos.add("resto5");
+        restos.add("resto6");
+
+
         if (android.os.Build.VERSION.SDK_INT > 9) {
 
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
@@ -73,8 +103,69 @@ public class RestoActivity extends AppCompatActivity
             StrictMode.setThreadPolicy(policy);
         }
 
-        String retour = GetProvinces();
-        Toast.makeText(getApplicationContext(),retour,Toast.LENGTH_LONG).show();
+
+        Spinner provinceSpinner = (Spinner) findViewById(R.id.spProvince);
+
+        //ajoute des provinces random dans la liste pour teste
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, provinces);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        provinceSpinner.setAdapter(adapter);
+
+        //le onChange du Spinner de province
+        provinceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            Spinner villeSpinner = (Spinner) findViewById(R.id.spCity);
+
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                villeSpinner.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                villeSpinner.setVisibility(View.INVISIBLE);
+            }
+
+        });
+
+        Spinner citySpinner = (Spinner) findViewById(R.id.spCity);
+        //ajoute des villes random dans la liste pour teste
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, cities);
+
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        citySpinner.setAdapter(adapter2);
+
+
+        //le onChange du Spinner de ville
+        citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            Spinner restoSpinner = (Spinner) findViewById(R.id.spResto);
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                restoSpinner.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                restoSpinner.setVisibility(View.INVISIBLE);
+            }
+
+        });
+
+        Spinner restoSpinner = (Spinner) findViewById(R.id.spResto);
+
+        //ajoute des provinces random dans la liste pour teste
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(
+                this, android.R.layout.simple_spinner_item, restos);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        restoSpinner.setAdapter(adapter3);
 
 
     }
@@ -105,10 +196,9 @@ public class RestoActivity extends AppCompatActivity
         return true;
     }
 
+    //va chercher la liste de provinces dans la bd
     public String GetProvinces() {
-
         RequestBody formBody = new FormBody.Builder()
-                .add("message", "Your message")
                 .build();
         Request request = new Request.Builder()
                 .url("http://192.168.1.112/api/Provinces/GetProvinces")
@@ -121,6 +211,41 @@ public class RestoActivity extends AppCompatActivity
             e.printStackTrace();
             return "Marche Pas";
         }
+    }
 
+    public String GetCities()
+    {
+        RequestBody formBody = new FormBody.Builder()
+                .add("ID", "ProvinceID")
+                .build();
+        Request request = new Request.Builder()
+                .url("http://192.168.1.112/api/Cities/GetCities")
+                .post(formBody)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            return response.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Marche Pas";
+        }
+    }
+
+    public String GetResto()
+    {
+        RequestBody formBody = new FormBody.Builder()
+                .add("ID", "RestoID")
+                .build();
+        Request request = new Request.Builder()
+                .url("http://192.168.1.112/api/Restaurants/GetRestaurants")
+                .post(formBody)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            return response.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Marche Pas";
+        }
     }
 }
